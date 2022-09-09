6c1f2a15192baf01112975e4d0808fd0ee888c4f
Samuel Amity <sam.amity@gmail.com>
2022-09-09 15:36:56 -0400

    initial pseudo code

---
+++ index.js
@@ -0,0 +1,13 @@
+/*Pseudo Code
+Event Listener for DOMContentLoaded
+
+Fetch request (get) to access db.json
+Write function to display fighters from db.json in a div container "character-select"
+Extract IDs from json to an array randomizer function iterates over
+    this function will check if randomly rolled number from 1-19 is present in the array
+
+
+Event listener on a randomize button (click) to return a random character name, and remove the character from the pool
+Event listener on a reset button (click) that restores all characters to the pool
+Event listener of dropdown menu (change) that changes page from light to dark mode
+*/
