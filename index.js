cd674ffcbd4e28ee4e5c18d6689162ef9d409adb
Samuel Amity <sam.amity@gmail.com>
2022-09-09 16:59:32 -0400

    variables defined

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-/*Pseudo Code
+/*
 Event Listener for DOMContentLoaded
 
 Fetch request (get) to access db.json
-Write function to display fighters from db.json in a div container "character-select"
-Extract IDs from json to an array randomizer function iterates over
+Write function to display fighters as cards from db.json in a div container "character-select"
+Extract IDs from json to an array randomizer function iterates over (maybe using map)
     this function will check if randomly rolled number from 1-19 is present in the array
 
 
@@ -13,11 +13,15 @@ Event listener of dropdown menu (change) that changes page from light to dark mo
 */
 
 let fighterArray = []
+const resetButton = document.getElementById("reset")
+const randomButton = document.getElementById("randomize")
+const dropdownThemes = document.getElementById("theme-dropdown")
 
 document.addEventListener("DOMContentLoaded", (grabFighters))
 
 function grabFighters() {
+    const fighterContainer = document.getElementById("character-select")
     fetch("http://localhost:3000/fighters")
     .then(response => response.json())
-    .then(response => console.log(response, "response"))
+    .then(response => console.log(response[0].id, "response"))
 }
