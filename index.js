8ff87131a98a3921b2c543e54a735247079dfa8a
Samuel Amity <sam.amity@gmail.com>
2022-09-10 11:07:07 -0400

    reset button functions

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
-/*
-Event Listener for DOMContentLoaded
-
-Fetch request (get) to access db.json
-Write function to display fighters as cards from db.json in a div container "character-select"
-Extract IDs from json to an array randomizer function iterates over (maybe using map)
-    this function will check if randomly rolled number from 1-19 is present in the array
-
-
-Event listener on a randomize button (click) to return a random character name, and remove the character from the pool
-Event listener on a reset button (click) that restores all characters to the pool
-Event listener of dropdown menu (change) that changes page from light to dark mode
-*/
 let fighterArray = []
-
+console.log(fighterArray);
 document.addEventListener("DOMContentLoaded", () => {
+    alert("RNGesus will... CHOOSE YOUR FIGHTER!!")
     grabFighters()
+    addFightersToArray()
     document.querySelector("#reset").addEventListener("click", () => {
         document.getElementById("character-select").innerHTML = "";
-        grabFighters();
+        fighterArray.length = 0
+        grabFighters()
+        addFightersToArray()
     })
     document.querySelector("#randomize").addEventListener("click", () => {
-
+        randomFighter()
+        console.log("random")
     })
-    const dropdownThemes = document.getElementById("theme-dropdown");
+    document.getElementById("theme-dropdown").addEventListener("change", (e) => {
+        if (e.target.value === "dark") {
+            document.querySelector("link[href='light.css']").href = "dark.css";
+        }
+        else {
+            document.querySelector("link[href='dark.css']").href = "light.css"
+        }
+        }
+    )
     }    
 );
 
@@ -31,6 +31,12 @@ function grabFighters() {
     .then(response => response.json())
     .then(data => data.forEach(fighter => showFighters(fighter)))
 };
+
+function addFightersToArray() {
+    fetch("http://localhost:3000/fighters")
+    .then(response => response.json())
+    .then(data => data.forEach(fighter => fighterArray.push(fighter.id)))
+}; 
 
 function showFighters(fighter) {
     const fighterContainer = document.getElementById("character-select");
@@ -44,10 +50,14 @@ function showFighters(fighter) {
     fighterContainer.append(div);
 };
 
-// randomButton.addEventListener("click", () => {
-//     console.log("ugh")
-// });
+// function randomFighterInt(min, max) {
+//     min = Math.ceil(1);
+//     max = Math.floor(19);
+//     return Math.floor(Math.random() * (19 - 1 + 1)) + 1;
+// }
 
-// dropdownThemes.addEventListener("change", () => {
-//     console.log("change")
-// });
+// function randomFighter() {
+//     var chosenFighter = fighterArray.random()
+//     fighterArray.remove(chosenFighter)
+//     console.log(fighterArray)
+// }
